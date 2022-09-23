472ad43abec5a3192d7e5c5e6c06d59ad3b8cf3d
Tomasz Lipiński <tomasz.lipinski@reality.co>
2022-09-23 23:21:31 +0200

    credit/debit

--- transfers/src/main/scala/net/tlipinski/moneytransfer/orchestrator/domain/MoneyTransfer.scala
+++ transfers/src/main/scala/net/tlipinski/moneytransfer/orchestrator/domain/MoneyTransfer.scala
@@ -10,16 +10,16 @@ import net.tlipinski.util.CodecConfiguration
 @JsonCodec
 case class MoneyTransfer(
     id: TransferId,
-    from: String,
-    to: String,
+    debited: String,
+    credited: String,
     amount: Int
 ) {
-  val changeBalanceFrom: ChangeBalance   = ChangeBalance(from, id, -amount)
-  val approveBalanceFrom: ApproveBalance = ApproveBalance(from, id)
-  val rejectBalanceFrom: RejectBalance   = RejectBalance(from, id)
+  val debitBalance: ChangeBalance         = ChangeBalance(debited, id, -amount)
+  val approveBalanceDebit: ApproveBalance = ApproveBalance(debited, id)
+  val rejectBalanceDebit: RejectBalance   = RejectBalance(debited, id)
 
-  val changeBalanceTo: ChangeBalance   = ChangeBalance(to, id, amount)
-  val approveBalanceTo: ApproveBalance = ApproveBalance(to, id)
-  val rejectBalanceTo: RejectBalance   = RejectBalance(to, id)
+  val creditBalance: ChangeBalance         = ChangeBalance(credited, id, amount)
+  val approveBalanceCredit: ApproveBalance = ApproveBalance(credited, id)
+  val rejectBalanceCredit: RejectBalance   = RejectBalance(credited, id)
 
 }
--- transfers/src/main/scala/net/tlipinski/moneytransfer/orchestrator/domain/MoneyTransferSaga.scala
+++ transfers/src/main/scala/net/tlipinski/moneytransfer/orchestrator/domain/MoneyTransferSaga.scala
@@ -9,41 +9,41 @@ object MoneyTransferSaga {
   val definition: SagaDefinition[MoneyTransfer, BankEvent, BankCommand] =
     SagaDefinition.create[MoneyTransfer, BankEvent, BankCommand](
       Step(
-        id = "transfer-money-to",
-        command = transfer => transfer.changeBalanceTo,
-        compensation = transfer => transfer.rejectBalanceTo.some,
+        id = "credit",
+        command = transfer => transfer.creditBalance,
+        compensation = transfer => transfer.rejectBalanceCredit.some,
         handleResponse = {
-          case (event: BalanceChanged, transfer) if matches(event, transfer, _.to) =>
+          case (event: BalanceChanged, transfer: MoneyTransfer) if matches(event, transfer, _.credited) =>
             SagaForward
         }
       ),
       Step(
-        id = "transfer-money-from",
-        command = transfer => transfer.changeBalanceFrom,
-        compensation = transfer => transfer.rejectBalanceFrom.some,
+        id = "debit",
+        command = transfer => transfer.debitBalance,
+        compensation = transfer => transfer.rejectBalanceDebit.some,
         handleResponse = {
-          case (event: BalanceChanged, transfer) if matches(event, transfer, _.from) =>
+          case (event: BalanceChanged, transfer: MoneyTransfer) if matches(event, transfer, _.debited) =>
             SagaForward
 
-          case (event: BalanceNotChanged, transfer) if matches(event, transfer, _.from) =>
+          case (event: BalanceNotChanged, transfer: MoneyTransfer) if matches(event, transfer, _.debited) =>
             SagaRollback
         }
       ),
       Step(
-        id = "approve-transfer-from",
-        command = transfer => transfer.approveBalanceFrom,
+        id = "approve-debit",
+        command = transfer => transfer.approveBalanceDebit,
         compensation = _ => none,
         handleResponse = {
-          case (event: BalanceApproved, transfer) if matches(event, transfer, _.from) =>
+          case (event: BalanceApproved, transfer: MoneyTransfer) if matches(event, transfer, _.debited) =>
             SagaForward
         }
       ),
       Step(
-        id = "approve-transfer-to",
-        command = transfer => transfer.approveBalanceTo,
+        id = "approve-credit",
+        command = transfer => transfer.approveBalanceCredit,
         compensation = _ => none,
         handleResponse = {
-          case (event: BalanceApproved, transfer) if matches(event, transfer, _.to) =>
+          case (event: BalanceApproved, transfer: MoneyTransfer) if matches(event, transfer, _.credited) =>
             SagaForward
         }
       )
