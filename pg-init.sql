aef6221a88c25081394c8bfc64b7d1ded5915fb4
tlipinski <me@tlipinski.net>
2023-09-16 00:29:41 +0200

    docker

--- pg-init.sql
+++ pg-init.sql
@@ -17,9 +17,11 @@ CREATE TABLE IF NOT EXISTS public.sagas
 CREATE TABLE IF NOT EXISTS public.outbox
 (
     id text COLLATE pg_catalog."default" NOT NULL,
-    topic text COLLATE pg_catalog."default" NOT NULL,
     key text COLLATE pg_catalog."default" NOT NULL,
     keyHash integer NOT NULL,
+    "timestamp" timestamp without time zone NOT NULL,
+    topic text COLLATE pg_catalog."default" NOT NULL,
+    sent boolean,
     message json NOT NULL,
     CONSTRAINT outbox_pkey PRIMARY KEY (id)
 );
